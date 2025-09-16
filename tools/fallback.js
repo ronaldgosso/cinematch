f8a480821df67095aad3a9f4ef80ff300d835daf
Ronald Isack Gosso <ronaldgosso@gmail.com>
2025-09-16 13:47:52 +0300

    increased tokens (transformers  offline)

--- tools/fallback.js
+++ tools/fallback.js
@@ -6,7 +6,7 @@ let input = `${globalVars.christianGuardrail}
 User: ${message}
 Assistant:
 `;
-const output = await generator(input, { max_new_tokens: 250 });
+const output = await generator(input, { max_new_tokens: 512 });
 return output;
 }
 
